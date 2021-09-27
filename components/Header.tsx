ab2d91e3cf3760e29866a64c791b7982e8b92c55
Mike Hogarth <mikehogarth20@hotmail.com>
2021-09-27 19:30:58 +0100

    UX on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,14 @@ export default function Header() {
   return (
     <>
       <header className="bg-primary text-white md:flex md:flex-row">
-        <h1 className="px-5 lg:px-20 xl:px-40 self-center md:tracking-widest md:min-w-max font-typewriter font-bold text-2xl md:text-4xl py-5 mb-0">
+        <h1 className="pl-5 lg:pl-20 xl:pl-40 self-center md:tracking-widest md:min-w-max font-typewriter font-bold text-2xl md:text-4xl py-5 mb-0">
           <Link href="/">
             <a className="mr-5">Green web dev</a>
           </Link>
 
           <button
             aria-label="Open Menu"
-            className="float-right md:hidden self-center mt-1"
+            className="float-right md:hidden self-center mt-1 mr-5 "
             onClick={(e) => setOpen(!open)}
           >
             <FaBars className="" />
@@ -24,33 +24,17 @@ export default function Header() {
         <nav
           className={`${
             open ? "block" : "hidden"
-          } md:inline-block px-5 lg:px-20 xl:px-40 bg-black md:bg-primary text-center md:text-right self-center w-full`}
+          } md:inline-block pr-5 lg:pr-20 xl:pr-40 bg-black md:bg-primary text-center md:text-right self-center w-full`}
         >
           <ul className="py-2 mt-0 md:mt-5 md:py-0 text-xl text-white list-none">
-            <li className="md:inline-block">
-              <NavLink
-                href="/about"
-                padRight={true}
-                text="About"
-                setOpen={setOpen}
-              />
-            </li>
-            <li className="md:inline-block">
-              <NavLink
-                href="/prompts"
-                padRight={true}
-                text="Prompts"
-                setOpen={setOpen}
-              />
-            </li>
-            <li className="md:inline-block">
-              <NavLink
-                href="/articles"
-                padRight={true}
-                text="Articles"
-                setOpen={setOpen}
-              />
-            </li>
+            <NavLink href="/about" text="About" setOpen={setOpen} />
+            <NavLink href="/prompts" text="Prompts" setOpen={setOpen} />
+            <NavLink
+              href="/articles"
+              text="Articles"
+              setOpen={setOpen}
+              supressBorder={true}
+            />
           </ul>
         </nav>
       </header>
@@ -61,24 +45,32 @@ export default function Header() {
 interface NavlinkProps {
   href: string;
   text: string;
-  padRight: boolean;
   setOpen: (isOpen: boolean) => void;
+  supressBorder?: boolean;
 }
 
-function NavLink({ href, text, padRight, setOpen }: NavlinkProps) {
+function NavLink({ href, text, supressBorder, setOpen }: NavlinkProps) {
   const router = useRouter();
   const pathname: string = router.pathname;
   const active: boolean = pathname === href || pathname.startsWith(`${href}/`);
 
-  const classes = `${active ? "active" : ""} ${padRight ? "mr-5" : ""}`;
+  const classes = `${active ? "active" : ""}`;
   return (
-    <Link href={href}>
-      <a
-        className={"my-4 md:my-0 border-white" + classes}
-        onClick={() => setOpen(false)}
-      >
-        {text}
-      </a>
-    </Link>
+    <li
+      className={`md:inline-block ${
+        supressBorder ? "" : "border-b-2"
+      } border-gray-800 md:border-none`}
+    >
+      <Link href={href}>
+        <a
+          className={
+            "block md:inline my-2 md:my-0 md:border-white md:ml-4" + classes
+          }
+          onClick={() => setOpen(false)}
+        >
+          {text}
+        </a>
+      </Link>
+    </li>
   );
 }
