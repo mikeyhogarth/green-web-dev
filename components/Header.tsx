fe9e9c4927058796a02c46520e6ce2d7b56d5b08
Mike Hogarth <mikehogarth20@hotmail.com>
2021-09-27 18:59:45 +0100

    Relent and add hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,60 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
   return (
-    <header className=" bg-primary text-white md:flex md:flex-row">
-      <h1 className="px-5 lg:px-20 xl:px-40 text-center md:text-left tracking-widest min-w-max font-typewriter font-bold text-4xl py-5 pb-0 md:pb-5 mb-0">
-        <Link href="/">
-          <a className="mr-5">Green web dev</a>
-        </Link>
-      </h1>
+    <>
+      <header className="bg-primary text-white md:flex md:flex-row">
+        <h1 className="px-5 lg:px-20 xl:px-40 md:self-center md:tracking-widest md:min-w-max font-typewriter font-bold md:text-4xl py-5 mb-0">
+          <Link href="/">
+            <a className="mr-5">Green web dev</a>
+          </Link>
 
-      <nav className="bg-secondary md:bg-primary text-center md:text-right self-center sm:block w-full pr-5 lg:pr-20 xl:pr-40">
-        <ul className="py-2 md:py-0 text-xl text-primary md:text-white list-none">
-          <li className="inline-block">
-            <NavLink href="/about" padRight={true} text="About" />
-          </li>
-          <li className="inline-block">
-            <NavLink href="/prompts" padRight={true} text="Prompts" />
-          </li>
-          <li className="inline-block">
-            <NavLink href="/articles" padRight={true} text="Articles" />
-          </li>
-          <li className="inline-block">
-            <NavLink href="/tldr" padRight={false} text="TLDR" />
-          </li>
-        </ul>
-      </nav>
-    </header>
+          <button
+            aria-label="Open Menu"
+            className="float-right md:hidden"
+            onClick={(e) => setOpen(!open)}
+          >
+            <FaBars className="" />
+          </button>
+        </h1>
+        <nav
+          className={`${
+            open ? "block" : "hidden"
+          } md:inline-block px-5 lg:px-20 xl:px-40 bg-black md:bg-primary text-center md:text-right self-center w-full`}
+        >
+          <ul className="py-2 mt-0 md:mt-5 md:py-0 text-xl text-white list-none">
+            <li className="md:inline-block">
+              <NavLink
+                href="/about"
+                padRight={true}
+                text="About"
+                setOpen={setOpen}
+              />
+            </li>
+            <li className="md:inline-block">
+              <NavLink
+                href="/prompts"
+                padRight={true}
+                text="Prompts"
+                setOpen={setOpen}
+              />
+            </li>
+            <li className="md:inline-block">
+              <NavLink
+                href="/articles"
+                padRight={true}
+                text="Articles"
+                setOpen={setOpen}
+              />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
 
@@ -34,9 +62,10 @@ interface NavlinkProps {
   href: string;
   text: string;
   padRight: boolean;
+  setOpen: (isOpen: boolean) => void;
 }
 
-function NavLink({ href, text, padRight }: NavlinkProps) {
+function NavLink({ href, text, padRight, setOpen }: NavlinkProps) {
   const router = useRouter();
   const pathname: string = router.pathname;
   const active: boolean = pathname === href || pathname.startsWith(`${href}/`);
@@ -44,7 +73,12 @@ function NavLink({ href, text, padRight }: NavlinkProps) {
   const classes = `${active ? "active" : ""} ${padRight ? "mr-5" : ""}`;
   return (
     <Link href={href}>
-      <a className={"border-primary md:border-white " + classes}>{text}</a>
+      <a
+        className={"my-4 md:my-0 border-white" + classes}
+        onClick={() => setOpen(false)}
+      >
+        {text}
+      </a>
     </Link>
   );
 }
