aa7e8149fc620854d41de1716e201b486e5c4808
Mike Hogarth <mikehogarth20@hotmail.com>
2021-10-02 12:11:58 +0100

    Fix a11y on secondary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extend: {
       colors: {
         primary: "#2E7D32",
-        secondary: "#A5D6A7",
+        secondary: "#C8E6C9",
         highlight: "#4CAF50",
       },
       fontSize: {
