01813c55ad9732901330732ad662410306f9e599
Mike Hogarth <mikehogarth20@hotmail.com>
2021-10-02 10:02:30 +0100

    Add PWA support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 const withMDX = require("@next/mdx")({
   extension: /\.mdx$/,
 });
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-});
+module.exports = withPWA(
+  withMDX({
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    pwa: {
+      disable: process.env.NODE_ENV === "development",
+      dest: "public",
+      runtimeCaching,
+    },
+  })
+);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-
+import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import HtmlHead from "../components/HtmlHead";
@@ -8,6 +8,12 @@ import HtmlHead from "../components/HtmlHead";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
       <HtmlHead
         title="Green Web Dev"
         description="Prompts and tips for creating sustainable web applications"
