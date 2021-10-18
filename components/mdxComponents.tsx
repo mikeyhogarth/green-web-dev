f7ce0e0e178cad372efc0c0aad2e7ab410c1a31f
Mike Hogarth <mikehogarth20@hotmail.com>
2021-10-18 09:31:54 +0100

    Linkify headers

--- components/mdxComponents.tsx
+++ components/mdxComponents.tsx
@@ -1,7 +1,29 @@
+import { FaLink } from "react-icons/fa";
+
 const components = {
   blockquote: function blockQuote(props: any) {
     return <aside {...props} />;
   },
+  h2: (props: any) => LinkifiedHeader("h2", props),
 };
 
 export default components;
+
+function LinkifiedHeader(HeadingComponent: any, props: any) {
+  if (typeof props.children !== "string")
+    return <HeadingComponent {...props} />;
+
+  const text = props.children;
+  const snakified = text.toLowerCase().replace(/ /g, "-");
+  const uri = `#${encodeURIComponent(snakified)}`;
+  return (
+    <>
+      <HeadingComponent {...props} id={snakified}>
+        {text}
+        <a href={uri} aria-hidden>
+          <FaLink className="ml-2 " style={{ fontSize: ".5em" }} aria-hidden />
+        </a>
+      </HeadingComponent>
+    </>
+  );
+}
